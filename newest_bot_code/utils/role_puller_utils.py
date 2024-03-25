53f36960a679334f36d805af0e8d998465d52102
the-jolley-boy <67850166+the-jolley-boy@users.noreply.github.com>
2024-03-25 13:25:26 -0700

    Add files via upload

--- newest_bot_code/utils/role_puller_utils.py
+++ newest_bot_code/utils/role_puller_utils.py
@@ -1,10 +1,9 @@
 import discord
 import os
 import asyncio
-import datetime
-from datetime import date
+from datetime import datetime
 import matplotlib.pyplot as plt
-import numpy as np
+import io
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -16,90 +15,6 @@ GUILD = int(os.environ.get('NOTIFYGUILDID'))
 NEWMEMBERROLE = int(os.environ.get('NEWMEMBERROLE'))
 STAFFMODCHANNEL = int(os.environ.get('STAFFMODCHANNEL'))
 
-Client = None
-
-# Sets client var on bot start
-async def set_vars(c):
-    global Client
-
-    Client = c
-
-# Generates the graphs and saves it
-async def graph(N, height1, height2, height3, tick_label, final_directory, end_directory):
-    ind = np.arange(N)
-    width = 0.25
-
-    fig = plt.figure(facecolor = "white", figsize = (21,9), dpi = 300)
-    #subplot to allow multiple plots
-    ax = fig.add_subplot(111)
-    ax2 = ax.twinx()
-    #all 3 sets of data into one plot (msg,word,char)
-    first = ax2.bar(ind, height1, width, color = ['aqua'])
-    second = ax.bar(ind+width, height2, width, color = ['lightgreen'])
-    third = ax.bar(ind+width+width, height3, width, color = ['fuchsia'])
-
-    #formatting stuff for the graph
-    ax.set_ylabel('Word/Char Totals')
-    ax2.set_ylabel('Msg Totals')
-    ax.set_title('Category Interaction Graph')
-    ax.set_xticks(ind + width / 3)
-    ax.set_xticklabels(tick_label)
-    ax.legend( (first[0], second[0], third[0]),("Tot Msgs", "Tot Words", "Tot Chars"),loc='upper center', bbox_to_anchor=(1,-0.1) )
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.set_facecolor("black")
-    #rotates the names 45 and changes font 
-    fig.autofmt_xdate(rotation = 45)
-    #fits everything to the borders
-    fig.tight_layout()
-
-    plt.savefig(final_directory + end_directory)
-
-async def fetch_data(category_name):
-    query = f"SELECT * FROM {category_name}"
-    records = await dbaccess.get_data(query, None)
-    ids, msgs, words, chars = [], [], [], []
-    for row in records:
-        ids.append(row[0])
-        msgs.append(row[2])
-        words.append(row[3])
-        chars.append(row[4])
-    return ids, msgs, words, chars
-
-async def fetch_user_names(ids, client):
-    names = []
-    for i in ids:
-        user = await client.fetch_user(i)
-        names.append(user.name)
-    return names
-
-async def export_data_to_csv(ids, msgs, words, chars, names, category_name, final_directory):
-    await graph(len(ids), msgs, words, chars, names, final_directory, f'/{category_name}/graph.png')
-    csv_filename = f'{final_directory}/{category_name}/csv.csv'
-    query = f"SELECT * FROM {category_name}"
-    records = await dbaccess.get_data(query, None)
-    column_names = ['nameid', 'name', 'msgcount', 'wordcount', 'charcount', 'engage']
-
-    if records:
-        # Write column headers to the CSV file
-        with open(csv_filename, 'w') as csv_file:
-            csv_file.write(",".join(column_names) + "\n")
-        # Write data rows to the CSV file
-        with open(csv_filename, 'a') as csv_file:
-            for row in records:
-                csv_file.write(",".join(map(str, row)) + "\n")
-    else:
-        print("Error with exporting to csv.")
-
-async def get_current_month():
-    query = "SELECT * FROM datetracker WHERE scriptid = 1"
-    records = await dbaccess.get_data(query, None)
-    for row in records:
-        if row[0] == 1:
-            return row[1]
-    return 0  # Return 0 if no record found
-
 async def text_data(msg):
     msgstring = msg.content
     words = len(msgstring.split())
@@ -109,16 +24,97 @@ async def text_data(msg):
 
     return  words, chars
 
-async def readwrite(stringtable, auth, a, count, words, chars, engage):
-    query = f"SELECT * FROM {stringtable} WHERE nameid = %s"
-    records = await dbaccess.get_data(query, (auth,))
+async def get_current_date_str():
+    curr_month = datetime.now().month
+    curr_year = str(datetime.now().year)[-2:]  # Get last two digits of the year
+    return f"{curr_month}_{curr_year}"
 
-    for row in records:
-        if row[0] == auth:
-            count += row[2]
-            words += row[3]
-            chars += row[4]
+async def readwrite(cat_id, auth_id, auth_name, msg, words, chars):
 
-    query = f"INSERT INTO {stringtable} (nameid, name, msgcount, wordcount, charcount, engage) VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (nameid) DO UPDATE set (name, msgcount, wordcount, charcount, engage) = (EXCLUDED.name, EXCLUDED.msgcount, EXCLUDED.wordcount, EXCLUDED.charcount, EXCLUDED.engage)"
-    data = (auth, str(a), count, words, chars, engage)
+    curr_date = await get_current_date_str()
+    await dbaccess.add_new_cols(curr_date)
+
+    # Construct the upsert query
+    query = f"""
+        INSERT INTO notify_staff_message_data (nameid, name, cat_id, msg_{curr_date}, word_{curr_date}, char_{curr_date})
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON CONFLICT (nameid, cat_id) DO UPDATE 
+        SET name = EXCLUDED.name,
+            msg_{curr_date} = notify_staff_message_data.msg_{curr_date} + EXCLUDED.msg_{curr_date},
+            word_{curr_date} = notify_staff_message_data.word_{curr_date} + EXCLUDED.word_{curr_date},
+            char_{curr_date} = notify_staff_message_data.char_{curr_date} + EXCLUDED.char_{curr_date};
+    """
+
+    # Data to be inserted or updated
+    data = (auth_id, str(auth_name), cat_id, msg, words, chars)
+
+    # Execute the query
     await dbaccess.write_data(query, data)
+
+async def messagedata(interaction, user):
+    staff_id = user
+
+    query = "SELECT * FROM notify_staff_message_data WHERE nameid = %s"
+    data = (staff_id,)
+
+    records = await dbaccess.get_data_role_puller(query, data)
+
+    if not records:
+        await interaction.response.send_message("No data found for the user.")
+        return
+
+    category_mapping = {
+        'Staff': 570142944921911296,
+        'Support Tickets': 570248275374899200, 
+        'Important': 570142946272477184,
+        'Releases': 622110241882112011,
+        'Sneakers': 1016090827073798164,
+        'Community': 570142948155588608, 
+        'Flips': 1099125615728283738
+    }
+
+    plt.figure(figsize=(16, 8))
+
+    colors = [
+        '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+        '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+        '#1a55FF', '#2aFF78', '#d4a028', '#c467bd', '#6c564b',
+        '#e7a1c2', '#4f9f7f', '#bc8d22', '#57aecf', '#9a77cf'
+    ]
+
+    for index, (cat_name, cat_id) in enumerate(category_mapping.items()):
+        for record in records:
+            if record['cat_id'] == cat_id:
+                dates = []
+                messages = []
+
+                for column_name, value in record.items():
+                    if column_name.startswith('msg_'):
+                        date_part = '_'.join(column_name.split('_')[1:])
+                        dates.append(date_part)
+                        messages.append(value)
+
+                # Plot each record as a separate line on the graph, using the color cycle
+                if dates and messages:
+                    plt.plot(dates, messages, marker='o', label=f"{cat_name}", color=colors[index % len(colors)])
+
+    plt.title('Message Activity')
+    plt.xlabel('Date')
+    plt.ylabel('Messages')
+    plt.grid(True)
+
+    # Place the legend outside the plot to the right with smaller text
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=3, fontsize='small')
+    plt.tight_layout()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', bbox_inches="tight")  # Ensure the external legend is included in the saved figure
+    buf.seek(0)
+    plt.close()
+
+    file = discord.File(fp=buf, filename='graph.png')
+    embed = discord.Embed(title="Message Activity")
+    embed.set_image(url="attachment://graph.png")
+    await interaction.channel.send(embed=embed, file=file)
+
+    buf.close()
